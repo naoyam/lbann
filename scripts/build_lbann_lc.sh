22a2be9bb2819d9e123daed8945f5374fc4c8a4b
Naoya Maruyama <maruyama3@llnl.gov>
2018-03-23 16:33:52 -0700

    Aggregated updates
    - Forward and backward convolution
    - Support 1x1 conv
    - Stride support
    - Support pooling
    - Support ReLU
    - Avoid copying between layers if possible
    - Skip when disabled
    - Temporary hack to use DISTCONV from LBANN
    - Support batch normalization
    - Enable deterministic pooling and some minor tweaks
    - Use the new shuffle copy routine in convolution and pooling
    - Update parallel strategies to record number of processes per replica for FC parallel strategies.
    - Use ParallelStrategy to set process grids
    - Fixes #3
    - Use correct mini batch sizes at the end of epochs
    - Fixes #17
    - Sample parallelization of convolution
    - Supports sample parallelization of ReLU
    - Supports sample parallelization of Pooling
    - Support sample parallelization of batch normalization
    - Hybrid of sample and spatial is also working.
    - Update nvtx profiler callback

--- scripts/build_lbann_lc.sh
+++ scripts/build_lbann_lc.sh
@@ -70,6 +70,7 @@ WITH_ALUMINUM=
 ALUMINUM_WITH_MPI_CUDA=OFF
 ALUMINUM_WITH_NCCL=
 WITH_CONDUIT=OFF
+WITH_DISTCONV=OFF
 WITH_TBINF=OFF
 RECONFIGURE=0
 USE_NINJA=0
@@ -267,6 +268,9 @@ while :; do
         --with-conduit)
             WITH_CONDUIT=ON
             ;;
+		--with-distconv)
+			WITH_DISTCONV=ON
+			;;
         --instrument)
             INSTRUMENT="-finstrument-functions -ldl"
             ;;
@@ -670,10 +674,9 @@ echo $COMPILER_VERSION
       echo "Set to the default CONDUIT_DIR="$CONDUIT_DIR
   fi
 fi
-################################################################
+
 # Setup Ninja, if using
 ################################################################
-
 if [ ${USE_NINJA} -ne 0 ]; then
     if ! which ninja ; then
         if [ "${ARCH}" == "x86_64" ]; then
@@ -686,6 +689,11 @@ if [ ${USE_NINJA} -ne 0 ]; then
         USE_NINJA=0
     fi
 fi
+
+# Temporary Distconv stuff
+################################################################
+DISTCONV_DIR=$HOME/lbann/install/$CLUSTER/$COMPILER/$BUILD_TYPE/distconv
+
 ################################################################
 # Display parameters
 ################################################################
@@ -791,6 +799,7 @@ CONFIGURE_COMMAND=$(cat << EOF
 -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
 -D CMAKE_INSTALL_MESSAGE=${CMAKE_INSTALL_MESSAGE} \
 -D CMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+-D CMAKE_CUDA_FLAGS_DEBUG="-G" \
 -D LBANN_SB_BUILD_CNPY=ON \
 -D LBANN_SB_BUILD_HYDROGEN=ON \
 -D LBANN_SB_FWD_HYDROGEN_Hydrogen_ENABLE_CUDA=${WITH_CUDA} \
@@ -821,6 +830,8 @@ CONFIGURE_COMMAND=$(cat << EOF
 -D LBANN_CONDUIT_DIR=${CONDUIT_DIR} \
 -D LBANN_BUILT_WITH_SPECTRUM=${WITH_SPECTRUM} \
 -D OPENBLAS_ARCH_COMMAND=${OPENBLAS_ARCH} \
+-D LBANN_WITH_DISTCONV=${WITH_DISTCONV} \
+-D LBANN_SB_FWD_LBANN_DISTCONV_DIR=${DISTCONV_DIR} \
 ${SUPERBUILD_DIR}
 EOF
 )
