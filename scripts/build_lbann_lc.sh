a9b7d9d65ed98f0a0b7f84f4daa1686d2411a5c0
Naoya Maruyama <maruyama3@llnl.gov>
2018-07-13 15:58:09 -0700

    Support P2P-based halo exchange

--- scripts/build_lbann_lc.sh
+++ scripts/build_lbann_lc.sh
@@ -677,6 +677,7 @@ fi
 # Temporary Distconv stuff
 ################################################################
 DISTCONV_DIR=$HOME/lbann/install/$CLUSTER/$COMPILER/$BUILD_TYPE/distconv
+P2P_DIR=$HOME/lbann/install/$CLUSTER/$COMPILER/$BUILD_TYPE/p2p
 
 ################################################################
 # Display parameters
@@ -819,6 +820,8 @@ CONFIGURE_COMMAND=$(cat << EOF
 -D OPENBLAS_ARCH_COMMAND=${OPENBLAS_ARCH} \
 -D LBANN_WITH_DISTCONV=${WITH_DISTCONV} \
 -D LBANN_SB_FWD_LBANN_DISTCONV_DIR=${DISTCONV_DIR} \
+-D LBANN_SB_FWD_LBANN_P2P_DIR=${P2P_DIR} \
+-D Aluminum_URL=git@github.com:ndryden/Aluminum.git \
 ${SUPERBUILD_DIR}
 EOF
 )
